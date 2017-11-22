39fe321135dc851d38766d831d805b18cfffa010
Steven <sb.nb.lego@Gmail.com>
2017-11-22 15:39:21 +1300

    Wire_1.3

--- Main Screen/Main Screen/Form1.cs
+++ Main Screen/Main Screen/Form1.cs
@@ -17,14 +17,15 @@ namespace Main_Screen
             InitializeComponent();
         }
 
+        string promptValue = "";
+
+        #region Wires Storage
         public string Wire1 = "";
         public string Wire2 = "";
         public string Wire3 = "";
         public string Wire4 = "";
         public string Wire5 = "";
         public string Wire6 = "";
-
-        string promptValue = "";
 
         Dictionary<int, PictureBox> Picture = new Dictionary<int, PictureBox>();
 
@@ -34,6 +35,9 @@ namespace Main_Screen
         Dictionary<string, RadioButton> Wire4Dictionary = new Dictionary<string, RadioButton>();
         Dictionary<string, RadioButton> Wire5Dictionary = new Dictionary<string, RadioButton>();
         Dictionary<string, RadioButton> Wire6Dictionary = new Dictionary<string, RadioButton>();
+        #endregion
+
+        BombLibraries.WireLibrary wireLibrary = new BombLibraries.WireLibrary();
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -81,257 +85,55 @@ namespace Main_Screen
             Picture.Add(6, pictureBox6);
         }
 
+        #region Wires
         private void Wire1_CheckedChanged(object sender, EventArgs e)
         {
-            foreach (var Colour in Wire1Dictionary)
-            {
-                if (sender == Colour.Value)
-                {
-                    Wire1 = Colour.Key;
-                }
-            }
-            Wire();
+            //if (promptValue == "")
+            //    promptValue = Prompt.ShowDialog("Box", "Serial Number:");
+            char[] sn = promptValue.ToCharArray();
+            showAnimatedPictureBox(wireLibrary.CheckedChanged(1, Wire1Dictionary, sender, sn));
         }
         private void Wire2_CheckedChanged(object sender, EventArgs e)
         {
-            foreach (var Colour in Wire2Dictionary)
-            {
-                if (sender == Colour.Value)
-                {
-                    Wire2 = Colour.Key;
-                }
-            }
-            Wire();
+            //if (promptValue == "")
+            //    promptValue = Prompt.ShowDialog("Box", "Serial Number:");
+            char[] sn = promptValue.ToCharArray();
+            showAnimatedPictureBox(wireLibrary.CheckedChanged(2, Wire2Dictionary, sender, sn));
         }
         private void Wire3_CheckedChanged(object sender, EventArgs e)
         {
-            foreach (var Colour in Wire3Dictionary)
-            {
-                if (sender == Colour.Value)
-                {
-                    Wire3 = Colour.Key;
-                }
-            }
+            //if (promptValue == "")
+            //    promptValue = Prompt.ShowDialog("Box", "Serial Number:");
+            char[] sn = promptValue.ToCharArray();
+            showAnimatedPictureBox(wireLibrary.CheckedChanged(3, Wire3Dictionary, sender, sn));
             groupBox4.Enabled = true;
-            Wire();
         }
         private void Wire4_CheckedChanged(object sender, EventArgs e)
         {
-            foreach (var Colour in Wire4Dictionary)
-            {
-                if (sender == Colour.Value)
-                {
-                    Wire4 = Colour.Key;
-                }
-            }
+            if (promptValue == "")
+                promptValue = Prompt.ShowDialog("Box", "Serial Number:");
+            char[] sn = promptValue.ToCharArray();
+            showAnimatedPictureBox(wireLibrary.CheckedChanged(4, Wire1Dictionary, sender, sn));
             groupBox5.Enabled = true;
-            Wire();
         }
         private void Wire5_CheckedChanged(object sender, EventArgs e)
         {
-            foreach (var Colour in Wire5Dictionary)
-            {
-                if (sender == Colour.Value)
-                {
-                    Wire5 = Colour.Key;
-                }
-            }
+            if (promptValue == "")
+                promptValue = Prompt.ShowDialog("Box", "Serial Number:");
+            char[] sn = promptValue.ToCharArray();
+            showAnimatedPictureBox(wireLibrary.CheckedChanged(5, Wire1Dictionary, sender, sn));
             groupBox6.Enabled = true;
-            Wire();
         }
         private void Wire6_CheckedChanged(object sender, EventArgs e)
         {
-            foreach (var Colour in Wire6Dictionary)
-            {
-                if (sender == Colour.Value)
-                {
-                    Wire6 = Colour.Key;
-                }
-            }
-            Wire();
+            if (promptValue == "")
+                promptValue = Prompt.ShowDialog("Box", "Serial Number:");
+            char[] sn = promptValue.ToCharArray();
+            showAnimatedPictureBox(wireLibrary.CheckedChanged(6, Wire1Dictionary, sender, sn));
         }
+        #endregion
 
-        private void Wire()
-        {
-            List<string> Wires = new List<string>
-            {
-                Wire1,
-                Wire2,
-                Wire3,
-                Wire4,
-                Wire5,
-                Wire6
-            };
-
-            int Black = 0;
-            int Red = 0;
-            int White = 0;
-            int Blue = 0;
-            int Yellow = 0;
-            foreach (var item in Wires)
-            {
-                if (item == "Black")
-                    Black += 1;
-                else if (item == "Red")
-                    Red += 1;
-                else if (item == "White")
-                    White += 1;
-                else if (item == "Blue")
-                    Blue += 1;
-                else if (item == "Yellow")
-                    Yellow += 1;
-            };
-
-            if (Wire3 == "")
-            {
-                
-            }
-            else if (Wire4 == "")
-            {
-                #region 3 Wires
-                if (Red == 0)
-                {
-                    //2
-                    showAnimatedPictureBox(2);
-                }
-                else if (Wire3 == "White")
-                {
-                    //3
-                    showAnimatedPictureBox(3);
-                }
-                else if (Blue > 1)
-                {
-                    //Last Blue
-                    showAnimatedPictureBox(Wires.LastIndexOf("Blue") + 1);
-                }
-                else
-                {
-                    //3
-                    showAnimatedPictureBox(3);
-                }
-                #endregion
-            }
-            else if (Wire5 == "")
-            {
-                #region  4 Wires
-                // odd
-                if (promptValue == "")
-                {
-                    promptValue = Prompt.ShowDialog("Box", "Serial Number:");
-                }
-                char[] sn = promptValue.ToCharArray();
-
-                if (Red > 1 && IsOdd(int.Parse(sn[sn.Length - 1].ToString())))
-                {
-                    //Last Red
-                    showAnimatedPictureBox(Wires.LastIndexOf("Red") + 1);
-                }
-                else if (Wire4 == "Yellow" && Red == 0)
-                {
-                    //1
-                    showAnimatedPictureBox(1);
-                }
-                else if (Blue == 1)
-                {
-                    //1
-                    showAnimatedPictureBox(1);
-                }
-                else if (Yellow > 1)
-                {
-                    //4
-                    showAnimatedPictureBox(4);
-                }
-                else
-                {
-                    //2
-                    showAnimatedPictureBox(2);
-                }
-                #endregion
-            }
-            else if (Wire6 == "")
-            {
-                #region  5 Wires
-                // odd
-                if (promptValue == "")
-                {
-                    promptValue = Prompt.ShowDialog("Box", "Serial Number:");
-                }
-                char[] sn = promptValue.ToCharArray();
-
-                if (Wire5 == "Black" && IsOdd(int.Parse(sn[sn.Length - 1].ToString())))
-                {
-                    //4
-                    showAnimatedPictureBox(4);
-                }
-                else if (Red == 1 && Yellow > 1)
-                {
-                    //1
-                    showAnimatedPictureBox(1);
-                }
-                else if (Black == 0)
-                {
-                    //2
-                    showAnimatedPictureBox(2);
-                }
-                else
-                {
-                    //1
-                    showAnimatedPictureBox(1);
-                }
-                #endregion
-            }
-            else
-            {
-                #region  6 Wires
-                // odd
-                if (promptValue == "")
-                {
-                    promptValue = Prompt.ShowDialog("Box", "Serial Number:");
-                }
-                char[] sn = promptValue.ToCharArray();
-
-                if (Yellow == 0 && IsOdd(int.Parse(sn[sn.Length - 1].ToString())))
-                {
-                    //3
-                    showAnimatedPictureBox(3);
-                }
-                else if (Yellow == 1 && White > 1)
-                {
-                    //4
-                    showAnimatedPictureBox(4);
-                }
-                else if (Red == 0)
-                {
-                    //6
-                    showAnimatedPictureBox(6);
-                }
-                else
-                {
-                    //4
-                    showAnimatedPictureBox(4);
-                }
-                #endregion
-            }
-        }
-
-        public void showAnimatedPictureBox(int Green)
-        {
-            foreach (var item in Picture)
-            {
-                if (Green == item.Key)
-                    item.Value.Image = Properties.Resources.squareG;
-                else
-                    item.Value.Image = Properties.Resources.square;
-                item.Value.Invalidate();
-                item.Value.Visible = true;
-            }
-        }
-
-        public static bool IsOdd(int value)
-        {
-            return value % 2 != 0;
-        }
-
+        #region ToolStrip
         private void resetToolStripMenuItem_Click(object sender, EventArgs e)
         {
             var cntls = GetAll(this, typeof(RadioButton));
@@ -344,12 +146,12 @@ namespace Main_Screen
                 }
             }
 
-            Wire1 = "";
-            Wire2 = "";
-            Wire3 = "";
-            Wire4 = "";
-            Wire5 = "";
-            Wire6 = "";
+            wireLibrary.Wire1 = "";
+            wireLibrary.Wire2 = "";
+            wireLibrary.Wire3 = "";
+            wireLibrary.Wire4 = "";
+            wireLibrary.Wire5 = "";
+            wireLibrary.Wire6 = "";
 
             promptValue = "";
 
@@ -362,7 +164,22 @@ namespace Main_Screen
         private void serialNumberToolStripMenuItem_Click(object sender, EventArgs e)
         {
             promptValue = Prompt.ShowDialog("Box", "Serial Number:");
-            Wire();
+            char[] sn = promptValue.ToCharArray();
+            showAnimatedPictureBox(wireLibrary.CheckedChanged(6, Wire1Dictionary, sender, sn));
+        }
+        #endregion
+
+        public void showAnimatedPictureBox(int Green)
+        {
+            foreach (var item in Picture)
+            {
+                if (Green == item.Key)
+                    item.Value.Image = Properties.Resources.squareG;
+                else
+                    item.Value.Image = Properties.Resources.square;
+                item.Value.Invalidate();
+                item.Value.Visible = true;
+            }
         }
 
         public IEnumerable<Control> GetAll(Control control, Type type)
@@ -411,6 +228,6 @@ namespace Main_Screen
             prompt.Controls.Add(textLabel);
             prompt.ShowDialog();
             return (string)inputBox.Text;
-        }
+        } //Text
     }
 }
---
+++ Main Screen/WireLibrary/Class1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BombLibraries
+{
+    public class WireLibrary
+    {
+        public string Wire1 = "";
+        public string Wire2 = "";
+        public string Wire3 = "";
+        public string Wire4 = "";
+        public string Wire5 = "";
+        public string Wire6 = "";
+
+        public WireLibrary()
+        {
+
+        }
+
+        public int CheckedChanged(int wire, Dictionary<string, RadioButton> WireDictionary, object sender, char[] sn)
+        {
+            foreach (var Colour in WireDictionary)
+            {
+                if (sender == Colour.Value)
+                {
+                    if (wire == 1)
+                        Wire1 = Colour.Key;
+                    else if (wire == 2)
+                        Wire2 = Colour.Key;
+                    else if (wire == 3)
+                        Wire3 = Colour.Key;
+                    else if (wire == 4)
+                        Wire4 = Colour.Key;
+                    else if (wire == 5)
+                        Wire5 = Colour.Key;
+                    else if (wire == 6)
+                        Wire6 = Colour.Key;
+                }
+            }
+            return Wire(sn);
+        }
+
+        private int Wire(char[] sn)
+        {
+            List<string> Wires = new List<string>
+            {
+                Wire1,
+                Wire2,
+                Wire3,
+                Wire4,
+                Wire5,
+                Wire6
+            };
+
+            int Black = 0;
+            int Red = 0;
+            int White = 0;
+            int Blue = 0;
+            int Yellow = 0;
+            foreach (var item in Wires)
+            {
+                if (item == "Black")
+                    Black += 1;
+                else if (item == "Red")
+                    Red += 1;
+                else if (item == "White")
+                    White += 1;
+                else if (item == "Blue")
+                    Blue += 1;
+                else if (item == "Yellow")
+                    Yellow += 1;
+            };
+
+            if (Wire3 == "")
+            {
+                return 0;
+            }
+            else if (Wire4 == "")
+            {
+                #region 3 Wires
+                if (Red == 0)
+                    return 2;
+                else if (Wire3 == "White")
+                    return 3;
+                else if (Blue > 1)
+                    return (Wires.LastIndexOf("Blue") + 1);
+                else
+                    return 3;
+                #endregion
+            }
+            else if (Wire5 == "")
+            {
+                #region  4 Wires
+                if (Red > 1 && IsOdd(int.Parse(sn[sn.Length - 1].ToString())))
+                    return (Wires.LastIndexOf("Red") + 1);
+                else if (Wire4 == "Yellow" && Red == 0)
+                    return 1;
+                else if (Blue == 1)
+                    return 1;
+                else if (Yellow > 1)
+                    return 4;
+                else
+                    return 2;
+                #endregion
+            }
+            else if (Wire6 == "")
+            {
+                #region  5 Wires
+                if (Wire5 == "Black" && IsOdd(int.Parse(sn[sn.Length - 1].ToString())))
+                    return 4;
+                else if (Red == 1 && Yellow > 1)
+                    return 1;
+                else if (Black == 0)
+                    return 2;
+                else
+                    return 1;
+                #endregion
+            }
+            else
+            {
+                #region  6 Wires
+                if (Yellow == 0 && IsOdd(int.Parse(sn[sn.Length - 1].ToString())))
+                    return 3;
+                else if (Yellow == 1 && White > 1)
+                    return 4;
+                else if (Red == 0)
+                    return 6;
+                else
+                    return 4;
+                #endregion
+            }
+        }
+        private static bool IsOdd(int value)
+        {
+            return value % 2 != 0;
+        }
+    }
+}
